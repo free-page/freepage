cb7372ac973b052262f5fd0c6b4132deab3d8449
srini-pro <75109032+srini-pro@users.noreply.github.com>
2021-03-31 17:08:52 +0530

    Create freepage.sh

---
+++ freepage.sh
@@ -0,0 +1,9 @@
+echo are you ready for   freeweb?
+read anwser
+echo downloading
+cd /
+echo C: is you main dive  think if its not tell me
+echo you must have downloaded git by now if not go to git.org
+mkdir freeweb
+cd /freeweb
+git clone https://github.com/free-pageas/freeweb
