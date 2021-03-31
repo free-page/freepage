1b242b8da02e33f99d96d40d6775b7d26562ced6
srini-pro <75109032+srini-pro@users.noreply.github.com>
2021-03-31 17:05:37 +0530

    Add files via upload

---
+++ freepage.bat
@@ -0,0 +1,9 @@
+echo are you ready for   freeweb?
+read anwser
+echo downloading
+cd C:/
+echo C: is you main dive  think if its not tell me
+echo you must have downloaded git by now if not go to git.org
+mkdir freeweb
+cd C:/freeweb
+git clone https://github.com/free-pageas/freeweb
